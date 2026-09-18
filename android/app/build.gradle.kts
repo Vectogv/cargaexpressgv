0f0b4aad90bbe7b1fec2d889b0c6f3b7f1318efa
Vector Antonimi Gv <107512262+Vectogv@users.noreply.github.com>
2026-09-18 17:39:07 -0500

    Actualizar build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,10 @@ plugins {
 
 android {
     namespace = "com.example.cargaexpress"
-    compileSdk = flutter.compileSdkVersion
+
+    // Se fuerza la compilación con Android SDK 36
+    compileSdk = 36
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -16,10 +19,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cargaexpress"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -28,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
